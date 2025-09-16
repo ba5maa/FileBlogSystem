0463a72eb36b263acca4e157c1eea3293b29ff3c
ba5maa <130751900+ba5maa@users.noreply.github.com>
2025-09-16 15:53:15 +0300

    print user and hashed password

--- FileBlogSystem/Endpoints/EndpointExtensions.cs
+++ FileBlogSystem/Endpoints/EndpointExtensions.cs
@@ -223,14 +223,14 @@ namespace FileBlogSystem.Endpoints
                 var user = await contentService.GetUserByUsernameAsync(request.Username);
                 if (user == null)
                 {
-                    return Results.Json(new { message = string.Format(EndpointConstants.InvalidUsernameOrPassword), Username = request.Username, Password = request.Password }, statusCode: 401);
+                    return Results.Json(new { message = string.Format(EndpointConstants.InvalidUsernameOrPassword), Username = request.Username, Password = request.Password, user = user }, statusCode: 401);
                 }
 
                 Console.WriteLine($"User found: {user.Username}. Stored hash: {user.HashedPassword}");
 
                 if (!PasswordHasher.VerifyPassword(request.Password, user.HashedPassword))
                 {
-                    return Results.Json(new { message = string.Format(EndpointConstants.InvalidUsernameOrPassword), Username = request.Username, Password = request.Password }, statusCode: 401);
+                    return Results.Json(new { message = string.Format(EndpointConstants.InvalidUsernameOrPassword), Username = request.Username, Password = request.Password, hashedPassword = user.HashedPassword }, statusCode: 401);
                 }
 
                 Console.WriteLine($"Login successful for user: {user.Username}. Generating token...");
